6bcb860162a60a42b63ad3b7e1a188dfbc174824
Victoriano Sevillano Vega <victorianosevillano@gmail.com>
2018-05-24 10:55:56 +0200

    Add files via upload

---
+++ Proyecto-Dwamana/php/contenido.php
@@ -0,0 +1,27 @@
+<?php 
+$seccion = $_POST["seccion"];
+
+//Controlador de las páginas
+switch($seccion){
+	case 'inicio': 
+		readfile('../php/inicio.html');
+	break;
+	case 'actividades': 
+		readfile('../php/actividades.html');
+	break;
+	case 'listado': 
+		readfile('../php/listado.html');
+		break;
+	case 'ponente': 
+		readfile('../php/ponente.html');
+	break;
+	case 'actividad': 
+		readfile('../php/actividad.html') ;
+	break;
+	case 'registro': 
+		readfile('../php/registro.html') ;
+	break;
+	case 'carteles': 
+		readfile('../php/carteles.html') ;
+	break;
+}
